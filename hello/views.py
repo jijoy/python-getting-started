d322d4d5ebe1cb6dab2df06da86397115870c59c
Jijoy <jijoy.chandrachoodan@whatfix.com>
2019-09-05 11:04:15 +0530

    First Version of the app

--- hello/views.py
+++ hello/views.py
@@ -8,6 +8,9 @@ def index(request):
     # return HttpResponse('Hello from Python!')
     return render(request, "index.html")
 
+def hello(request):
+    # return HttpResponse('Hello from Python!')
+    return render(request, "hello.html")
 
 def db(request):
 
